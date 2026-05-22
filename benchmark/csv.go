575e28d7a318ca0dc2e20cc4b30d190bf35631b2
Han-16 <haanbk16@gmail.com>
2026-05-22 11:31:53 +0900

    chore: format sub-second benchmark times in ms

--- benchmark/csv.go
+++ benchmark/csv.go
@@ -331,7 +331,7 @@ func formatSeconds(seconds float64) string {
 }
 
 func FormatDurationSeconds(seconds float64) string {
-	if math.Abs(seconds) < 0.005 {
+	if math.Abs(seconds) < 1 {
 		return fmt.Sprintf("%dms", int(math.Round(seconds*1000)))
 	}
 	return fmt.Sprintf("%.2fs", seconds)
